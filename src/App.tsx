baa2f0570e224e97e5d0eb3c2dd5dedcc0bf03c8
Thien Trinh <thien.trinh@hcl.com>
2022-09-15 14:08:59 +0700

    Minor format

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import Layout from './components/UI/layout';
 import Home from './components/page/Home';
 
 function App() {
-  return <Routes>
-    <Route path='/' element={<Layout/>}>
-      <Route index element={<Home/>}/>
-    </Route>
-  </Routes>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
